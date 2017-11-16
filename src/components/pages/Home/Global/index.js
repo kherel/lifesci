3c1770d144a42e2d62af5cc0ebaf8ce93037bba7
Alexandr Adamant <alexandradamant@gmail.com>
2017-11-16 14:48:14 +0300

    [update] index content

--- src/components/pages/Home/Global/index.js
+++ src/components/pages/Home/Global/index.js
@@ -1,43 +1,61 @@
-import React from 'react';
+import React from 'react'
 // import * as T from "prop-types";
-import './styles.scss';
-import {cssClassName} from 'utils'
+import './styles.scss'
+import { cssClassName } from 'utils'
 const cn = cssClassName('Global')
 import A_H from 'A_H'
 import A_P from 'A_P'
 import A_Container from 'A_Container'
 import A_Image from 'A_Image'
 
-const Global = () =>{
-  return(
-    <A_Container type='section' mx={cn()}>
+const Global = () => {
+  return (
+    <A_Container type="section" mx={cn()}>
       <div className={cn('left')}>
-        <A_H type='section' mx={cn('title')}>Lifesci Global Platform </A_H>
-        <A_P type='section' mx={cn('text')}>The platform includes the latest industry reports and specific data analytics in order to understand and apprehend the health sector.</A_P>
+        <A_H type="section" mx={cn('title')}>
+          Lifesci Global Platform
+        </A_H>
+        <A_P type="section" mx={cn('text')}>
+          An independent, secure scalable platform, that reduces expensive IT set up costs and
+          offering a quick ready solution.
+        </A_P>
 
         <section className={cn('small-section')}>
-          <A_H type='small-section'>Life Index Network</A_H>
-          <A_P type='second'>Lifesci holds an exclusive portfolio of healthcare and life science companies. The access to these  companies will be given to the network of participants possessing the Lifesci Token. </A_P>
+          <A_H type="small-section">Master patient index</A_H>
+          <A_P type="second">
+            A master patient index is a database that is used across a healthcare organisation to
+            maintain consistent, accurate and current demographic and essential medical data on the
+            patients seen and managed within its various departments, aggregated, stored and
+            analysed at Lifesci
+          </A_P>
         </section>
         <section className={cn('small-section')}>
-          <A_H type='small-section'>Life Index Network</A_H>
-          <A_P type='second'>Lifesci holds an exclusive portfolio of healthcare and life science companies. The access to these  companies will be given to the network of participants possessing the Lifesci Token. </A_P>
+          <A_H type="small-section">Lifesci Fund</A_H>
+          <A_P type="second">
+            A non regulated vehicle that will potentially invest in the equity of a range of
+            companies in the Biotech, healthcare and life science sector. It will look to provide
+            capital to assist in the financing of products and devices that are at different stages
+            of evaluation and pharmacological assessment.
+          </A_P>
         </section>
       </div>
       <div className={cn('right')}>
-        <A_Image src={'/img/photo/sir-martin-evans.jpg'} alt='Sir Martin Evans' objectFit='contain' mx={cn('photo')}/>
+        <A_Image
+          src={'/img/photo/sir-martin-evans.jpg'}
+          alt="Sir Martin Evans"
+          objectFit="contain"
+          mx={cn('photo')}
+        />
         <div className={cn('label')}>
-          <A_P type='name'>Sir Martin Evans</A_P>
-          <A_P type='title'>Lifesci’s chief advisor</A_P>
+          <A_P type="name">Sir Martin Evans</A_P>
+          <A_P type="title">Lifesci’s chief advisor</A_P>
         </div>
       </div>
     </A_Container>
   )
 }
 
-Global.propTypes = {
-};
+Global.propTypes = {}
 
-Global.defaultProps = {
-}
+Global.defaultProps = {}
 export default Global
--- src/components/pages/Home/Platform/index.js
+++ src/components/pages/Home/Platform/index.js
@@ -1,51 +1,78 @@
-import React from 'react';
+import React from 'react'
 // import * as T from "prop-types";
-import './styles.scss';
-import {cssClassName} from 'utils'
+import './styles.scss'
+import { cssClassName } from 'utils'
 import A_Container from 'A_Container'
 const cn = cssClassName('Platform')
 import A_H from 'A_H'
 import A_P from 'A_P'
 import A_Image from 'A_Image'
 
-const Platform = () =>{
-  return(
-    <A_Container type='equal' mx={cn()}>
+const Platform = () => {
+  return (
+    <A_Container type="equal" mx={cn()}>
       <div className={cn('left')}>
-        <A_H type='section' mx={cn('title')}>The Intelligence Platform</A_H>
-        <A_P type='section' mx={cn('text')}>
-          The platform includes the latest industry reports and specific data analytics in order to understand and apprehend the health sector.
+        <A_H type="section" mx={cn('title')}>
+          The Intelligence Platform
+        </A_H>
+        <A_P type="section" mx={cn('text')}>
+          The platform includes the latest industry reports and specific data analytics in order to
+          understand and apprehend the health sector.
         </A_P>
 
-        <section className={cn('section', {icon: 'lines'})}>
-          <A_H type='small-section'>Intrepreneurs and Investors</A_H>
-          <A_P type='second'>Lifesci holds an exclusive portfolio of healthcare and life science companies. The access to these  companies will be given to the network of participants possessing the Lifesci Token. </A_P>
+        <section className={cn('section', { icon: 'lines' })}>
+          <A_H type="small-section">Intrepreneurs and Investors</A_H>
+          <A_P type="second">
+            Currently there is no accessible independent database for those that wish to monitor and
+            potentially take advantage of investment opportunities in this fast growing sector.
+            According to PwC the life science sector is worth over £30 Billion in the UK alone and
+            as life expectancy continues to expand there will be increasing demand for new medical
+            breakthroughs.
+          </A_P>
           <div className={cn('docs')}>
-            <A_P type='second' mx={cn('docs-item')}>Feature exclusive content about healthcare industry</A_P>
-            <A_P type='second' mx={cn('docs-item')}>Interviews with thought leaders</A_P>
-            <A_P type='second' mx={cn('docs-item')}>The Healthcare Labz interviews to spotlight the great people</A_P>
-            <A_P type='second' mx={cn('docs-item')}>Bring thought-provoking ideas</A_P>
+            <A_P type="second" mx={cn('docs-item')}>
+              Data is stored in a high secure manner on a blockchain
+            </A_P>
+            <A_P type="second" mx={cn('docs-item')}>
+              Neural network analyses of the data on the platform
+            </A_P>
+            <A_P type="second" mx={cn('docs-item')}>
+              Independent data on new medical breakthroughs
+            </A_P>
+            <A_P type="second" mx={cn('docs-item')}>
+              Investment opportunities available via crowdfund or a pooled investment vehicle
+            </A_P>
           </div>
         </section>
-        <section className={cn('section', {icon: 'shield'})}>
-          <A_H type='small-section'>Doctors & Healthcare Professionals</A_H>
-          <A_P type='second'>Lifesci holds an exclusive portfolio of healthcare and life science companies. The access to these  companies will be given to the network of participants possessing the Lifesci Token. </A_P>
+        <section className={cn('section', { icon: 'shield' })}>
+          <A_H type="small-section">Doctors & Healthcare Professionals</A_H>
+          <A_P type="second">
+            A depository for studies and field tests, a location to promote what they are doing if
+            they are looking for others to collaborate on trials and research. Easy to access data
+            and research paper so helping professionals in the sector with their CPD as well as a
+            reference point for those studying for their individual qualifications.{' '}
+          </A_P>
         </section>
       </div>
       <div className={cn('right')}>
-        <A_Image src={'/img/illustrations/tablet.svg'} mx={cn('illustration')}/>
-        <section className={cn('section', {icon: 'note'})}>
-          <A_H type='small-section'>Academics and Researchers</A_H>
-          <A_P type='second'>Lifesci holds an exclusive portfolio of healthcare and life science companies. The access to these  companies will be given to the network of participants possessing the Lifesci Token. Lifesci holds an exclusive portfolio of healthcare and life science companies. The access to these  companies will be given to the network of participants possessing the Lifesci Token. </A_P>
+        <A_Image src={'/img/illustrations/tablet.svg'} mx={cn('illustration')} />
+        <section className={cn('section', { icon: 'note' })}>
+          <A_H type="small-section">Academics and Researchers</A_H>
+          <A_P type="second">
+            To monitor what other universities are doing and so decide whether to pursue similar
+            areas of research, a central database that is secure when collaborating with other
+            institutions. A quick and and cost effective way to share and hold research data where
+            results and findings can be kept private or disseminated widely. Scalable so able to
+            store data and results from small studies to those involving many different
+            contributors.
+          </A_P>
         </section>
       </div>
     </A_Container>
   )
 }
 
-Platform.propTypes = {
-};
+Platform.propTypes = {}
 
-Platform.defaultProps = {
-}
+Platform.defaultProps = {}
 export default Platform
