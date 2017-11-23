f42e17d498025fce060151edca8624387d8ab4bc
Alexandr Adamant <alexandradamant@gmail.com>
2017-11-23 09:31:21 +0300

    [fix] prettify routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import {Route, IndexRoute, IndexRedirect} from 'react-router'
+import { Route, IndexRoute, IndexRedirect } from 'react-router'
 import React from 'react'
 import MainLayout from './MainLayout'
 import Documents from './Documents'
@@ -13,73 +13,29 @@ import Test from './test2'
 
 const getRoutes = () => {
   return (
-    <Route
-      path="/"
-      component={MainLayout}
-      prepareData={MainLayout.prepareData}
-    >
-      <IndexRoute
-        component={Home}
-      />
-      <Route
-        path="/whitepaper"
-        component={Documents}
-      />
-      <Route
-        path="/contact-us"
-        component={ContactUs}
-      />
-      <Route
-        path="/team"
-        component={Team}
-      />
-      <Route
-        path="contribution"
-        component={Contribution}
-      />
-      <Route
-        path="faq"
-        component={Faq}
-      />
+    <Route path="/" component={MainLayout} prepareData={MainLayout.prepareData}>
+      <IndexRoute component={Home} />
+      <Route path="/whitepaper" component={Documents} />
+      <Route path="/contact-us" component={ContactUs} />
+      <Route path="/team" component={Team} />
+      <Route path="contribution" component={Contribution} />
+      <Route path="faq" component={Faq} />
       {getLegal()}
-      <Route
-        path="/test"
-        component={Test}
-      />
-      <Route
-        path="/roadmap"
-        component={Roadmap}
-      />
-    </Route >
+      <Route path="/test" component={Test} />
+      <Route path="/roadmap" component={Roadmap} />
+    </Route>
   )
 }
 
-
 export default getRoutes
 
-
-
 const getLegal = () => (
-  <Route
-    path="/legal"
-  >
+  <Route path="/legal">
     <IndexRedirect to="business-terms-of-service" />
 
-    <Route
-      path="business-terms-of-service"
-      component={Legal}
-    />
-    <Route
-      path="supplements"
-      component={Legal}
-    />
-    <Route
-      path="complaints-policy"
-      component={Legal}
-    />
-    <Route
-      path="cookies-policy"
-      component={Legal}
-    />
+    <Route path="business-terms-of-service" component={Legal} />
+    <Route path="supplements" component={Legal} />
+    <Route path="complaints-policy" component={Legal} />
+    <Route path="cookies-policy" component={Legal} />
   </Route>
 )
