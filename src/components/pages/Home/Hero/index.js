97be6a1b737929612837d38b5c9c4de5e8d148a8
Alexandr Adamant <alexandradamant@gmail.com>
2017-11-23 09:33:57 +0300

    [fix] home page hero element styling

--- src/components/pages/Home/Hero/index.js
+++ src/components/pages/Home/Hero/index.js
@@ -168,15 +168,11 @@ class Hero extends Component {
           <canvas ref={canvas => this.canvas = canvas} width='500' height='500' className={cn('canvas')}/>
 
           <A_H type="hero" mx={cn('title')}>Solving life sciences real problems of today.</A_H>
-
-          <div>
-            <A_P type="hero" mx={cn('text')}>Lifesci offers access to valuable licence fee income and a blockchain AI driven platform that gives investor access to the latest insights, news in the healthcare industry.</A_P>
-            <div className={cn('buttons')}>
-              <A_Button type='button-secondary' mx={cn('buttons-subscribe')} onClick={handleClick}>Subscribe me</A_Button>
-              <A_Button type='link-primary' mx={'buttons-contribute'} to='/contribution'>How to Contribute</A_Button>
-            </div>
+          <A_P type="hero" mx={cn('text')}>Lifesci offers access to valuable licence fee income and a blockchain AI driven platform that gives investor access to the latest insights, news in the healthcare industry.</A_P>
+          <div className={cn('buttons')}>
+            <A_Button type='button-secondary' mx={cn('buttons-subscribe')} onClick={handleClick}>Subscribe me</A_Button>
+            <A_Button type='link-primary' mx={'buttons-contribute'} to='/contribution'>How to Contribute</A_Button>
           </div>
-
 
         </A_Container>
       </div>
