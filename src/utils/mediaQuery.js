b3d9cdaa83a15a4a1172bdca1492d09f4e01b1e8
Alexandr Adamant <alexandradamant@gmail.com>
2017-11-24 12:35:35 +0300

    [fix] wrong ssr window object call

--- src/utils/mediaQuery.js
+++ src/utils/mediaQuery.js
@@ -1,5 +1,10 @@
 export default function mediaQuery(mobile, tablet = mobile, desktop = tablet) {
-  if (window.matchMedia('(min-width: 1040px)').matches) return desktop
-  if (window.matchMedia('(min-width: 768px)').matches) return tablet
-  return mobile
+  if (__CLIENT__) {
+    if (window.matchMedia('(min-width: 1040px)').matches) return desktop
+    if (window.matchMedia('(min-width: 768px)').matches) return tablet
+    return mobile
+  } else {
+    return mobile
+  }
+
 }
