383d7d6521337e3e2bfea8e03258b78d7a50a571
Kherel Kechil <kherel@gmail.com>
2017-11-28 13:04:41 +0800

    [add] simple ref check

--- src/routes/Read.js
+++ src/routes/Read.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import ReadContainer from 'Read/container'
+import history from 'history'
 
 class HomeRoute extends Component {
+  componentWillMount(){
+    const arrayRefs = [111, 222, 333, 444, 555]
+    const {ref} = this.props.location.query
+    const accessAllowed = arrayRefs.some( el => +ref === el)
+    console.log(accessAllowed, ref)
+    if(!accessAllowed){
+      history.push('/')
+    }
+  }
   render() {
+    const {ref} = this.props.location.query
+    console.log(ref)
     return <ReadContainer />
   }
 }
